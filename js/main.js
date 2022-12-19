cd33497e7b1a80159d413caff6fbc56b4054ee83
Plamic14 <danielmarek66@gmail.com>
2022-12-19 16:35:12 +0100

    edited ham menu

--- js/main.js
+++ js/main.js
@@ -31,6 +31,8 @@ function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
+    navClose.classList.remove("show-close")
+    navToggle.classList.remove("hide-toggle")
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
